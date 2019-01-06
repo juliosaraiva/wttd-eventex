0b6e5190c9589ede177697ee2aeaca1a367add0a
Julio Saraiva <contato@juliosaraiva.com.br>
2019-01-06 04:10:48 -0200

    test

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -4,16 +4,16 @@ from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today')
+    list_display = ('name', 'email', 'phone', 'cpf', 'created_at')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
     list_filter = ('created_at',)
 
-    def subscribed_today(self, obj):
-        return obj.created_at == now().date()
+    # def subscribed_today(self, obj):
+    #     return obj.created_at == now().date()
 
-    subscribed_today.short_description = 'Inscrito Hoje?'
-    subscribed_today.boolean = True
+    # subscribed_today.short_description = 'Inscrito Hoje?'
+    # subscribed_today.boolean = True
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
