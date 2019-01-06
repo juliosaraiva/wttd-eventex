e9ae615da4e6093f6eb87ed925bb9f3fbe945219
Julio Saraiva <contato@juliosaraiva.com.br>
2019-01-06 16:45:26 -0200

    subscribed_today

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -4,7 +4,7 @@ from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'cpf', 'created_at')
+    list_display = ('name', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
     list_filter = ('created_at',)
