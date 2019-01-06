bebe6df5ba448c4657ad4725a6e89bf162d1dfc8
Julio Saraiva <contato@juliosaraiva.com.br>
2019-01-06 08:24:26 -0200

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -45,8 +45,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -76,11 +76,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name="Julio Saraiva", cpf="91963230272",
-                    email="contato@juliosaraiva.com.br", phone="21-99162-8287")
-        response = self.client.post("/inscricao/", data, follow=True)
-        self.assertContains(response, "Inscrição Realizada com Sucesso!")
