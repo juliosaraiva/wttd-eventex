955d068a0054885dc57aab16f56eda06bf30a1b5
Julio Saraiva <contato@juliosaraiva.com.br>
2020-08-26 06:11:49 -0300

    feature: class-based-view generic to send email

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,50 +1,10 @@
-from django.conf import settings
-from django.contrib import messages
-from django.core import mail
 from django.views.generic import DetailView
-from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import render, resolve_url as r
-from django.template.loader import render_to_string
-from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
+from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.mixins import EmailCreateView
 
-
-def new(request):
-    if request.method == 'POST':
-        return create(request)
-
-    return empty_form(request)
-
-
-def empty_form(request):
-    return render(request, 'subscriptions/subscription_form.html',
-                  {'form': SubscriptionForm()})
-
-
-def create(request):
-    form = SubscriptionForm(request.POST)
-
-    if not form.is_valid():
-        return render(request, 'subscriptions/subscription_form.html',
-                      {"form": form})
-
-    subscription = form.save()
-
-    _send_email('Confirmação de Inscrição',
-                settings.DEFAULT_FROM_EMAIL,
-                subscription.email,
-                'subscriptions/subscription_email.txt',
-                {'subscription': subscription})
-
-    return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
-
+new = EmailCreateView.as_view(model=Subscription,
+                              form_class=SubscriptionForm,
+                              email_subject='Confirmação de Inscrição')
 
 detail = DetailView.as_view(model=Subscription)
-
-
-def _send_email(subject, from_, to, template_name, context):
-    body = render_to_string(template_name, context)
-    mail.send_mail(subject,
-                   body,
-                   from_,
-                   [from_, to])
